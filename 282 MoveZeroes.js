73d233ade973075d502a456830e9f1476fdf4076
NCZ <497128887@qq.com>
2018-05-30 23:36:45 +0800

    add 282 description

--- 282 MoveZeroes.js
+++ 282 MoveZeroes.js
@@ -7,6 +7,7 @@ var moveZeroes = function(nums) {
         i = 0;
     // 时间复杂度O(n),空间复杂度O(1)
     // 移动时就进行交换
+    // 遍历到i个元素，[0,i]中，保证[0,k)均为非0元素，[k,i]均为0
     for (i; i < nums.length; i++) {
         if (nums[i]) {
             if (i != k) {
