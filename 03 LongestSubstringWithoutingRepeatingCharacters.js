f89bd17df2bcb6bc6c246235e2655c518abb7e4c
NCZ <497128887@qq.com>
2018-06-03 20:11:53 +0800

    update 03

--- 03 LongestSubstringWithoutingRepeatingCharacters.js
+++ 03 LongestSubstringWithoutingRepeatingCharacters.js
@@ -2,42 +2,25 @@
  * @param {string} s
  * @return {number}
  */
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var lengthOfLongestSubstring = function(s) {
-    let strArr = [];
-    let maxLen = 0;
-    console.log(s, s.length);
-    if (s.length && s.length > 1) {
-        strArr = s.split("");
-    } else if (s.length <= 1) {
-        // console.log(s.length);
-        return s.length;
-    }
-    let begin = 0; //某个点开始比较
-    let index = 1; //这个点，要与从begin开始到index-1的点比较
-    let temp = 0; //标记位，
-    let len = 1; //如果begin到index-1比较中，没有相等的case，+1
-    while (index < strArr.length) {
-        len = 1;
-        for (begin; begin < index; begin++) {
-            if (strArr[begin] == strArr[index]) {
-                maxLen = ((index - begin) > maxLen) ? (index - begin) : maxLen;
-                begin++; //起始点变为相等的下一位
-                temp = begin; //标记初始起始点；如果一轮比下来都没有相等话，index++之后还要从temp这个初始点开始比较
-                index++;
-                // console.log(begin,index);
-                break;
-            } else {
-                len++;
-                maxLen = (len > maxLen) ? len : maxLen;
-                if (begin + 1 == index) {
-                    begin = temp; //一轮比下来都没有相等，那index++之后，begin的位置还是要在这个for循环起始的begin位置
-                    index++;
-                    break;
-                }
-            }
+    // let freq = Array.apply(null, Array(256)).map(() => 0);
+    let freq = {};
+    let l = 0,
+        r = -1; // [l,r]为滑动窗口
+    let ret = 0;
+    while (l < s.length) {
+        if ((r + 1) < s.length && !freq[s[r + 1]]) {
+            r++;
+            freq[s[r]] = true;
+            ret = Math.max(ret, r - l + 1);
+        } else {
+            freq[s[l]] = false;
+            l++;
         }
     }
-    // console.log(maxLen);
-    return maxLen;
-
-};
+    return ret;
+}
