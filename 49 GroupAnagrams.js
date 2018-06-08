f4372b6f14bfe54465dc0029491524d1dd5bd380
NCZ <497128887@qq.com>
2018-06-08 21:11:57 +0800

    update 49

--- 49 GroupAnagrams.js
+++ 49 GroupAnagrams.js
@@ -3,44 +3,16 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    // 超时版本，最古老的版本了
-    if (strs.length < 1) {
-        return [];
-    }
-    let ret = [
-        [strs[0]]
-    ];
-    for (let i = 1; i < strs.length; i++) {
-        let j = 0;
-        for (j; j < ret.length; j++) {
-            if (isAnagram(ret[j][0], strs[i])) {
-                ret[j].push(strs[i]);
-                break;
-            }
-        }
-        if (j == ret.length) {
-            ret.push([strs[i]]);
-        }
-    }
-    return ret;
-};
-
-var isAnagram = function(s, t) {
-    let m = {};
-    if (s.length != t.length) {
-        return false;
-    }
-    s.split('').forEach(element => {
-        m[element] ? m[element]++ : (m[element] = 1)
-    });
-
-    for (const iterator of t.split('')) {
-        if (m[iterator]) {
-            m[iterator]--;
+    let ret = [];
+    let hash = {};
+    strs.forEach(str => {
+        temp = str.split('').sort().join('');
+        if (hash[temp] != undefined) {
+            ret[hash[temp]].push(str);
         } else {
-            return false;
+            hash[temp] = ret.length;
+            ret.push([str])
         }
-    }
-    // js中forEach无法break，return
-    return true;
+    });
+    return ret;
 };
