1a326e17ac5ccb70ace3d9c878b01523ad12f005
NCZ <497128887@qq.com>
2018-06-12 21:16:53 +0800

    add 328

---
+++ 328 OddEvenLinkedList.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    if (head == null) return head;
+    let odd = head;
+    let even = head.next;
+    let evenHead = head.next;
+    while (even && even.next) {
+        odd.next = even.next;
+        odd = odd.next;
+        even.next = odd.next;
+        even = even.next;
+    }
+    odd.next = evenHead;
+    return head;
+};
