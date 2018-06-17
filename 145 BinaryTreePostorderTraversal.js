bf29da1adfb3ded9fbaad6bce2a8f34c6cdb2bcd
NCZ <497128887@qq.com>
2018-06-17 23:08:20 +0800

    add 145 Iterative Solution Of BinaryTree Postorder Traversal

--- 145 BinaryTreePostorderTraversal.js
+++ 145 BinaryTreePostorderTraversal.js
@@ -19,3 +19,27 @@ var postorderTraversal = function(root) {
     }
     return ret;
 };
+
+// 迭代解法
+var postorderTraversal = function(root) {
+    let ret = [];
+    let stack = [];
+    if (root == null) return ret;
+    stack.push(new Command('go', root));
+    while (stack.length && stack[stack.length - 1]) {
+        let cmd = stack.pop();
+        if (cmd.name == 'print') {
+            ret.push(cmd.node.val);
+        } else if (cmd.name == 'go') {
+            stack.push(new Command('print', cmd.node));
+            if (cmd.node.right != null) stack.push(new Command('go', cmd.node.right));
+            if (cmd.node.left != null) stack.push(new Command('go', cmd.node.left));
+        }
+    }
+    return ret;
+}
+
+function Command(name, node) {
+    this.name = name;
+    this.node = node;
+}
