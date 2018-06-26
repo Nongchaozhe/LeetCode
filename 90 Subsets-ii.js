b1e50c924293d3cdddcdc35ba595469f0d85c2fe
NCZ <497128887@qq.com>
2018-06-26 22:13:31 +0800

    update 90

--- 90 Subsets-ii.js
+++ 90 Subsets-ii.js
@@ -4,7 +4,7 @@
  */
 var subsetsWithDup = function(nums) {
     nums.sort((a, b) => { return a - b; });
-    let set = new Set();
+    // let set = new Set(); //去重也可以通过s.toString()利用set的性质达到目的
     let ret = [];
     for (let i = 0; i <= nums.length; i++) {
         generateSubsets(nums, 0, i, []);
@@ -12,12 +12,12 @@ var subsetsWithDup = function(nums) {
     return ret;
 
     function generateSubsets(nums, start, len, s) {
-        if (s.length == len && !set.has(s.toString())) {
-            set.add(s.toString());
-            ret.push(s.slice(0));
+        if (s.length == len) {
+            ret.push([...s]);
             return;
         }
         for (let i = start; i < nums.length; i++) {
+            if (i > start && nums[i] === nums[i - 1]) continue; //同一层不出现相同元素
             if (s.length < len) {
                 s.push(nums[i]);
                 generateSubsets(nums, i + 1, len, s);
