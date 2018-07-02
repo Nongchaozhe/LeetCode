49f3615b0ff4f079ea74e2f0a15dd7c6a96c78eb
NCZ <497128887@qq.com>
2018-07-02 22:37:40 +0800

    update 198

--- 198 HouseRobber.js
+++ 198 HouseRobber.js
@@ -2,7 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-// 记忆搜索-超时
+// 动态规划
+var rob = function(nums) {
+    console.assert(nums != null && nums.length != 0, "nums can't be null")
+    let memo = new Array(nums.length + 1);
+    memo[0] = nums[0];
+    if (nums.length == 1) {
+        return memo[0];
+    }
+    memo[1] = Math.max(nums[0], nums[1]);
+    for (let i = 2; i < nums.length; i++) {
+        memo[i] = Math.max(memo[i - 1], nums[i] + memo[i - 2]);
+    }
+    return memo[nums.length - 1];
+};
+
+
+// 记忆搜索 - 超时
 var rob = function(nums) {
     let memo = new Array(nums.length + 1);
     let max = generateMaxRob(nums, 0);
