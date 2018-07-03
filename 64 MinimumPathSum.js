5d8cecf5376ede3683d5a4eea4db77c1b622f72a
NCZ <497128887@qq.com>
2018-07-03 22:59:10 +0800

    add 64

---
+++ 64 MinimumPathSum.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+    let x = grid.length;
+    let y = grid[0].length;
+
+    let dp = new Array(x);
+    for (let i = 0; i < x; i++)
+        dp[i] = new Array(y);
+
+    dp[0][0] = 1;
+    for (let i = 0; i < x; i++) {
+        for (let j = 0; j < y; j++) {
+            let value = grid[i][j];
+            if (i == 0 && j == 0) continue;
+            else if (i == 0 && j > 0) dp[i][j] = value + dp[i][j - 1];
+            else if (i > 0 && j == 0) dp[i][j] = value + dp[i - 1][j];
+            else if (i > 0 && j > 0) dp[i][j] = value + Math.min(dp[i][j - 1], dp[i - 1][j]);
+        }
+    }
+    return dp[x - 1][y - 1];
+};
