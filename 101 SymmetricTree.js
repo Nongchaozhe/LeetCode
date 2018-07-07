f672d73ceb44e570d95ab4c0d04d63333195ad24
NCZ <497128887@qq.com>
2018-07-07 22:26:39 +0800

    update 101

--- 101 SymmetricTree.js
+++ 101 SymmetricTree.js
@@ -10,25 +10,11 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-    if (root === null) return true;
-    root.left = invertTree(root.left);
-    return sameTree(root.left, root.right);
+    return mirrorTree(root, root);
 };
 
-function sameTree(p, q) {
-    if (p === null && q === null) return true;
-    if (p === null || q === null) return false;
-    if (p.val === q.val) {
-        return sameTree(p.left, q.left) && sameTree(p.right, q.right);
-    }
-    return false;
-}
-
-function invertTree(root) {
-    if (root === null) return null;
-    let left = invertTree(root.left);
-    let right = invertTree(root.right);
-    root.left = right;
-    root.right = left;
-    return root;
+function mirrorTree(l, r) {
+    if (l == null && r == null) return true;
+    if (l == null || r == null) return false;
+    return l.val === r.val && mirrorTree(l.right, r.left) && mirrorTree(l.left, r.right);
 }
