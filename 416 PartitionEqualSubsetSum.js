2bcb5e6c9a8fb7593d103aaa72d5725f74b884c8
NCZ <497128887@qq.com>
2018-07-11 22:41:54 +0800

    update 416 动态规划

--- 416 PartitionEqualSubsetSum.js
+++ 416 PartitionEqualSubsetSum.js
@@ -45,3 +45,28 @@ var canPartition1 = function(nums) {
         return memo[index][sum];
     }
 };
+
+// 动态规划
+var canPartition = function(nums) {
+    let sum = 0;
+    nums.forEach(item => {
+        sum += item;
+    });
+    if (sum % 2 !== 0) return false;
+
+    // memo[i][j] 为-1未计算过，0计算过不能填充，1计算过可以填充
+    let memo = new Array(sum / 2 + 1);
+    // id为0的物品，能否填满容量为id的背包
+    for (let i = 0; i <= sum / 2; i++) {
+        memo[i] = (nums[0] === i);
+    }
+
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = sum / 2; j >= nums[i]; j--) {
+            // 第i个物品不放入 || 第一个物品放入
+            memo[j] = memo[j] || memo[j - nums[i]];
+        }
+    }
+
+    return memo[sum / 2];
+};
