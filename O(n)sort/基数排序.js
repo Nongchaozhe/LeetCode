6bf2fcd95f9921c4044c987e88bfb80b2a6a9a3a
NCZ <497128887@qq.com>
2019-08-30 23:15:28 +0800

    add 基数排序

---
+++ O(n)sort/基数排序.js
@@ -0,0 +1,32 @@
+function radixSort(arr, maxDigit) {
+    let mod = 10;
+    let dev = 1;
+    let counter = [];
+    for (let i = 0; i < maxDigit; i++, dev *= 10, mod *= 10) {
+        for (let j = 0; j < arr.length; j++) {
+            let bucketIdx = parseInt((arr[j] % mod) / dev);
+            if (counter[bucketIdx] == null) {
+                counter[bucketIdx] = [];
+            }
+
+            counter[bucketIdx].push(arr[j]);
+        }
+
+        let idx = 0;
+        for (let j = 0; j < counter.length; j++) {
+            let bucket = counter[j];
+            if (bucket != null) {
+                for (let k = 0; k < bucket.length; k++) {
+                    arr[idx++] = bucket[k];
+                }
+            }
+        }
+
+        counter = []; //清空
+    }
+}
+
+var array = [12, 123, 18, 18, 30, 1, 0, 1];
+radixSort(array, 3);
+// radixSort1(array, 3);
+console.log(array);
